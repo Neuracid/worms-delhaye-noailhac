bef6635e3a8dc8d32d786eb89c58a1f8fcd8c644
JeanGuy15 <guillaume.noailhac@gmail.com>
2015-10-29 10:37:11 +0100

    rapport + os + MapOut
    début du rapport pour la section du rendu
    amélioration de la détection de l’OS pour le ./configure
    juste un problème de la fonction MapOut

--- src/Etat.cpp
+++ src/Etat.cpp
@@ -1,4 +1,23 @@
 #include "Etat.hpp"
 
-Etat::Etat(int nombreJoueur,int nombreWorms,int hauteur,int largeur):v(nombreWorms*nombreJoueur),grille( hauteur, largeur){
+Etat::Etat(int nombreJoueur,int nombreWorms,int hauteur,int largeur):v(nombreWorms*nombreJoueur),grille( hauteur, largeur),listeBloc(hauteur,largeur){
+}
+
+void parseOutMap(){
+    std::ofstream fichier("../res/MapOut.txt", std::ios::out | std::ios::trunc);
+    int largeur,hauteur;
+    largeur=this.grille.getLargeur();
+    hauteur=this->grille.getHauteur();
+
+    if(fichier){
+      fichier<<hauteur<<' '<<largeur<<std::endl;
+      for(int i=0;i<hauteur;i++){
+        for(int j=0;j<largeur;j++)
+        fichier<<this->listeBloc[j+i*largeur]<<' ';
+        fichier<<std::endl;
+      }
+       fichier.close();
+    }
+    else
+            std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
 }
--- src/Etat.hpp
+++ src/Etat.hpp
@@ -1,15 +1,19 @@
 #include <vector>
 #include "WormsJoueur.hpp"
+#include "ListeBloc.hpp"
 #include "Grille.hpp"
 #include <ctime>
 #include <iostream>
+#include <fstream>
 
 class Etat{
 public:
   std::vector<WormsJoueur> v;
+  ListeBloc listeBloc;
   Grille grille;
   std::time_t result = std::time(NULL);
   Etat(int nombreWorms,int nombreJoueur,int hauteur,int largeur);
+  void parseOutMap();
 protected:
 
 private:
