dc359960eb10fb9ea693737168ad351dbfa0fd1d
JeanGuy15 <guillaume.noailhac@gmail.com>
2015-11-03 14:22:13 +0100

    ggg

--- src/SfmlWorms.hpp
+++ src/SfmlWorms.hpp
@@ -3,6 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <fstream>
+#include <vector>
 
 class SfmlWorms : public sf::Drawable, public sf::Transformable
 {
@@ -11,12 +12,14 @@ public:
   SfmlWorms(int largeur, int hauteur, int nombreDeWorms);
   ~SfmlWorms();
   bool load(const std::string& tileset, sf::Vector2u tileSize, unsigned int x, unsigned int y,int numeroWorms);
-
+  int nombreDeWorms;
   int* tab;
   int nombre;
   int **tabWorms;
   void parseTxt1();
   void parseTxt();
+  std::vector<sf::Text> text;
+  std::vector<sf::Texture> texture;
 
 private:
 
