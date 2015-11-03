228f22c9872edcf265ea85b316d5f7aac30332ca
Neuracid <Neuracide@MNeuracide.local>
2015-11-03 14:55:19 +0100

    SfWorms

--- src/SfmlWorms.cpp
+++ src/SfmlWorms.cpp
@@ -1,9 +1,7 @@
 #include "SfmlWorms.hpp"
 
 //Constructeur , destructeur
-SfmlWorms::SfmlWorms(int largeur, int hauteur,int nombreDeWorms) : texture(nombreDeWorms), text(nombreDeWorms){
-nombre=largeur*hauteur;
-tab=new int[nombre];
+SfmlWorms::SfmlWorms(int nombreDeWorms) : texture(nombreDeWorms), text(nombreDeWorms){
 this->nombreDeWorms=nombreDeWorms;
 tabWorms=new int* [nombreDeWorms];
 for(int i;i<nombreDeWorms;i++)
@@ -17,52 +15,32 @@ SfmlWorms::~SfmlWorms(){
 }
 
 //Méthodes
-bool SfmlWorms::load(const std::string& tileset, sf::Vector2u tileSize, unsigned int width, unsigned int height,int numeroWorms)
+bool SfmlWorms::load(const std::string& tileset, sf::Vector2u tileSize, int positionTabWorms)
 {
+    // on charge la texture du tileset
+    if (!m_tileset.loadFromFile(tileset))
+        return false;
 
 
 
 
+            // on en déduit sa position dans la texture du tileset
+            int tileNumber = tabWorms[positionTabWorms][3];
 
+            // on récupère un pointeur vers le quad à définir dans le tableau de vertex
+            sf::Vertex* quad = &quads[positionTabWorms];
 
+            // on définit ses quatre coins
+            quad[0].position = sf::Vector2f(10, 10);
+            quad[1].position = sf::Vector2f(110, 10);
+            quad[2].position = sf::Vector2f(110, 110);
+            quad[3].position = sf::Vector2f(10, 110);
 
-
-
-
-    // // on charge la texture du tileset
-    // if (!m_tileset.loadFromFile(tileset))
-    //     return false;
-    //
-    // // on redimensionne le tableau de vertex pour qu'il puisse contenir tout le niveau
-    // m_vertices.setPrimitiveType(sf::Quads);
-    // m_vertices.resize(nombreDeWorms * 5);
-    //
-    // // on remplit le tableau de vertex, avec un quad par tuile
-    // for (unsigned int i = 0; i < width; ++i)
-    //     for (unsigned int j = 0; j < height; ++j)
-    //     {
-    //         // on récupère le numéro de tuile courant
-    //         int tileNumber = tab[i + j * width];
-    //
-    //         // on en déduit sa position dans la texture du tileset
-    //         int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-    //         int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
-    //
-    //         // on récupère un pointeur vers le quad à définir dans le tableau de vertex
-    //         sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
-    //
-    //         // on définit ses quatre coins
-    //         quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
-    //         quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-    //         quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
-    //         quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
-    //
-    //         // on définit ses quatre coordonnées de texture
-    //         quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
-    //         quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-    //         quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
-    //         quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-    //     }
+            // on définit ses quatre coordonnées de texture
+            quad[0].texCoords = sf::Vector2f(0, 0);
+            quad[1].texCoords = sf::Vector2f(25, 0);
+            quad[2].texCoords = sf::Vector2f(25, 50);
+            quad[3].texCoords = sf::Vector2f(0, 50);
 
     return true;
 }
@@ -89,25 +67,7 @@ bool SfmlWorms::load(const std::string& tileset, sf::Vector2u tileSize, unsigned
 //   return true;
 // }
 
-void SfmlWorms::parseTxt(){
-  std::ifstream fichier("../res/txt/worms.txt", std::ios::in);
-  if(fichier)
-  {
-
-    int rec;
-    for(int i=0; i<nombre; i++){
-      fichier >> rec;
-      tab[i]=rec;
-    }
-    fichier.close();
-  }
-  else
-  {
-    std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
-  }
-}
-
- void SfmlWorms::parseTxt1(){
+ void SfmlWorms::parseTxt(){
    std::ifstream fichier("../res/txt/WormsOut.txt", std::ios::in);
    if(fichier)
    {
--- src/SfmlWorms.hpp
+++ src/SfmlWorms.hpp
@@ -9,24 +9,17 @@ class SfmlWorms : public sf::Drawable, public sf::Transformable
 {
 public:
 
-  SfmlWorms(int largeur, int hauteur, int nombreDeWorms);
+  SfmlWorms(int nombreDeWorms);
   ~SfmlWorms();
   bool load(const std::string& tileset, sf::Vector2u tileSize, unsigned int x, unsigned int y,int numeroWorms);
   int nombreDeWorms;
-  int* tab;
-  int nombre;
   int **tabWorms;
-  void parseTxt1();
   void parseTxt();
   std::vector<sf::Text> text;
   std::vector<sf::Texture> texture;
 
 private:
 
-
   virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-  sf::VertexArray m_vertices;
-
-  sf::Texture m_tileset;
 };
