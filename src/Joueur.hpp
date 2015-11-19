cc4745bfd0acccd18e67062228631ecc6971ebeb
Neuracide <thomasdelhaye1@gmail.com>
2015-11-19 15:26:18 +0100

    l

--- src/Joueur.hpp
+++ src/Joueur.hpp
@@ -3,11 +3,12 @@
 class Joueur{
 
 public:
+
   enum Team{rouge=0,jaune=1,vert=2,bleu=3};
   Joueur(Team a);
   ~Joueur();
- Team getTeam();
- int getVie();
+  Team getTeam();
+  int getVie();
 private:
   int vieEquipe;
   Team team;
